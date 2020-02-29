d8446692312886b335e13f97bb4eba31b19c3eb5
corngo <carloscornejo96@ymail.com>
2020-02-29 02:49:40 -0600

    Added commands

--- CorngoBot/Commands.cs
+++ CorngoBot/Commands.cs
@@ -19,10 +19,42 @@ namespace CorngoBot
 
             // build out the reply
             sb.AppendLine($"You are -> [" + user.Username + "]");
-            sb.AppendLine("I must now say, World!");
 
             // send simple string reply
             await ReplyAsync(sb.ToString());
+        }
+
+        [Command("tomoko")]
+        public async Task Tomoko()
+        {
+            String[] tomoko = System.IO.Directory.GetFiles(AppDomain.CurrentDomain.BaseDirectory + @"\tomoko\");
+            Random rnd = new Random();
+            int rndImg = rnd.Next(0, tomoko.Length);
+
+            var channel = Context.Channel;
+            await channel.SendFileAsync(tomoko[rndImg]);
+
+            //var guild = Context.Guild.//_client.Guilds.Single(g => g.Name == "guild name");
+            //var channel = guild.TextChannels.Single(ch => ch.Name == "channel name");
+            
+            //await channel.SendFileAsync("D:\\Corngo\\Pictures\\IMG_3186.png", "Caption goes here");
+        }
+
+        [Command("vsauce")]
+        public async Task VSauce()
+        {
+            String[] vsauce = System.IO.Directory.GetFiles(AppDomain.CurrentDomain.BaseDirectory + @"\vsauce\");
+            Random rnd = new Random();
+            int rndImg = rnd.Next(0, vsauce.Length);
+
+            var channel = Context.Channel;
+            await channel.SendFileAsync(vsauce[rndImg]);
+        }
+
+        [Command("jose")]
+        public async Task Jose()
+        {
+            await Context.Channel.SendMessageAsync("fuck Jose", true);
         }
     }
 }
